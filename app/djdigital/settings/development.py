862807763260e73154fd93f9ba9e3e6e6d2c16bb
mostofakamalrasel <mdmostofakamal@protonmail.com>
2020-02-22 22:14:41 +0600

    :package: Postgresql - Added postgresql configuration in settings

--- app/djdigital/settings/development.py
+++ app/djdigital/settings/development.py
@@ -18,9 +18,20 @@ WSGI_APPLICATION = 'djdigital.wsgi.application'
 ##    DATABASE CONFIGURATION  ##
 ################################
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config('SQL_ENGINE'),
+        'NAME': config('SQL_DATABASE'),
+        'USER': config('SQL_USER'),
+        'PASSWORD': config('SQL_PASSWORD'),
+        'HOST': config('SQL_HOST'),
+        'PORT': config('SQL_PORT'),
     }
 }
