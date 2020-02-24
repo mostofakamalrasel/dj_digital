97df97f5362d5fc5521e72f4c45541c556e2db7a
mostofakamalrasel <mdmostofakamal@protonmail.com>
2020-02-24 14:59:38 +0600

    Re-added porduction docker files

--- app/djdigital/settings/production.py
+++ app/djdigital/settings/production.py
@@ -20,8 +20,12 @@ WSGI_APPLICATION = 'djdigital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config('SQL_ENGINE'),
+        'NAME': config('SQL_DATABASE'),
+        'USER': config('SQL_USER'),
+        'PASSWORD': config('SQL_PASSWORD'),
+        'HOST': config('SQL_HOST'),
+        'PORT': config('SQL_PORT'),
     }
 }
 
